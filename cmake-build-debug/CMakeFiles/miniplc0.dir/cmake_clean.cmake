830e0ccb6900e24b3eb29019ccf8e7d914be3285
Jongjyh <522963890@qq.com>
2019-11-25 19:50:52 +0800

    ee

---
+++ cmake-build-debug/CMakeFiles/miniplc0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miniplc0.dir/main.cpp.obj"
+  "miniplc0.pdb"
+  "miniplc0.exe"
+  "miniplc0.exe.manifest"
+  "libminiplc0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/miniplc0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
