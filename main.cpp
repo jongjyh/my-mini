1261292aa0e639c9309db1353757f8b9190ee415
Jongjyh <522963890@qq.com>
2019-12-17 13:11:39 +0800

    ee

--- main.cpp
+++ main.cpp
@@ -36,9 +36,9 @@ void Analyse(std::istream& input, std::ostream& output){
 	}
 	auto v = p.first;
     std::vector<std::pair<std::string,int>> Consts=v.getConstList();
-    std::vector<Function> funlist=v.getFunctionList();
-    std::vector<Instruction> beginCode=getBeginCode();
-    std::vector<std::vector<Instruction>> program=v.getProgramList();
+    std::vector<miniplc0::Function> funlist=v.getFunctionList();
+    std::vector<miniplc0::Instruction> beginCode=v.getBeginCode();
+    std::vector<std::vector<miniplc0::Instruction>> program=v.getProgramList();
 
     output << fmt::format(".constants:\n");
     for(int i=0;i<Consts.size();i++){
